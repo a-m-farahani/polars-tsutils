cc8edcc778887f796d0467204bbaa7c8c12dcd60
Ali Farahani <farahaniali@outlook.com>
2026-06-13 16:41:49 +0000

    [project]: add interpolate methods to polars-psutils  __init__.

--- src/polars_tsutils/__init__.py
+++ src/polars_tsutils/__init__.py
@@ -1,10 +1,16 @@
 from .gaps import detect_gaps, flag_gaps, coverage
+from .interpolate import fill_zoh, seed_at_boundary
 
 __all__ = [
     # gaps
     "detect_gaps",
     "flag_gaps",
     "coverage",
+
+    # interpolate
+    "fill_zoh",
+    "seed_at_boundary",
+
 ]
 
 __version__ = "0.0.1"
